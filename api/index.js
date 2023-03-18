d485648afa0508d0b19f617e9d74ea30c402c0ac
Ali Hanafiah <hanafiaha308@gmail.com>
2023-03-18 22:13:11 +0700

    menambah root update untuk pelatihan

--- api/index.js
+++ api/index.js
@@ -68,6 +68,7 @@ app.delete("/api/delete/mahasiswa", async (_req, res) => {
     await client.query(`delete from mahasiswa`);
     res.send("Berhasil Menghapus Data Mahasiswa semuanya");
 });
+
 app.delete("/api/delete/mahasiswa/:id", async (req, res) => {
     await client.query(`delete from mahasiswa where id = ${req.params.id}`);
     res.send("Berhasil Menghapus Data satu Mahasiswa");
@@ -85,4 +86,10 @@ app.post("/api/tambah/pelatihan", async (req, res) => {
     await client.query(`insert into pelatihan  values (DEFAULT,'${req.body.nama}')`)
     res.send("Berhasil Menabah Data pelatihan");
 });
+
+app.put("/api/update/pelatihan",async (req,res)=>{
+    await client.query(`update pelatihan set nama =' ${req.body.nama}'`);
+    res.send("Berhasil mengganti nama");
+});
+
 app.listen(3000);
